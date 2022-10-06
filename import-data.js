84465ef83d7147dcf5bbd9a28902907a8377a4b7
dianekrych <85934723+dianekrych@users.noreply.github.com>
2022-10-06 19:55:21 +0200

    marche pas

--- import-data.js
+++ import-data.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 require('dotenv').config()
 const { Schema } = mongoose;
 
+mongoose.connect(process.env.MONGO_URI).then((success)=> console.log("connecté à mongoDB"))
+
 const FilmLocationSchema = new Schema({
     filmType: String,
     filmProducerName: String,
@@ -23,5 +25,28 @@ const FilmLocationSchema = new Schema({
 
 const Location = mongoose.model('Location', FilmLocationSchema);
 
+const filmingLocations = require('./lieux-de-tournage-a-paris.json');
+console.log(filmingLocations.length);
 
-mongoose.connect(process.env.MONGO_URI).then((success)=> console.log("connecté à mongoDB"))
+function CreationLocation(filmingLocations){
+    for (const location in filmingLocations){
+        const lieu = new Location({
+            filmType: filmingLocations[location].fields.type_tournage,
+            filmProducerName: filmingLocations[location].fields.nom_producteur,
+            endDate: filmingLocations[location].fields.date_fin,
+            filmName: filmingLocations[location].fields.nom_tournage,
+            district: filmingLocations[location].fields.ardt_lieu,
+            geolocation: {
+                coordinates: filmingLocations[location].fields.geo_shape.coordinates,
+                type: filmingLocations[location].fields.geo_shape.type
+            },
+            sourceLocationId: filmingLocations[location].fields.id_lieu,
+            filmDirectorName: filmingLocations[location].fields.nom_realisateur,
+            address: filmingLocations[location].fields.adresse_lieu,
+            startDate: filmingLocations[location].fields.date_debut,
+            year: filmingLocations[location].fields.annee_tournage,
+        })
+    }
+}
+
+//CreationLocation(filmingLocations)
